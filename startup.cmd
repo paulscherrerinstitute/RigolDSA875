9eb3151a8cbf86b696dbb07a013810d385ed8829
Saso Skube <saso.skube@psi.ch>
2016-12-13 09:40:17 +0100

    Bug fix: missing default port in startup script

--- startup.cmd
+++ startup.cmd
@@ -4,5 +4,5 @@ epicsEnvSet ("STREAM_PROTOCOL_PATH", "$(RigolDSA875_DIR):$(STREAM_PROTOCOL_PATH=
 
 drvAsynIPPortConfigure("$(PORT=RIG1)", "$(IP):$(SCPI_PORT=5555)")
 
-dbLoadTemplate("$(RigolDSA875_TEMPLATES)/SpectAnly.subs", "S=$(SYS), STRM_PORT=$(PORT)")
+dbLoadTemplate("$(RigolDSA875_TEMPLATES)/SpectAnly.subs", "S=$(SYS), STRM_PORT=$(PORT=RIG1)")
 ## END of configuration
